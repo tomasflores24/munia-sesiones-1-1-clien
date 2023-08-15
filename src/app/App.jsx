d42b4af17ed8ae1ffa693a6284e4a70b5fb26edc
galoss88 <gallogarcianicolas@gmail.com>
2023-08-15 14:25:32 -0300

    quitado registar de app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,11 @@
 import { Provider } from "react-redux";
 import "../App.css";
-import Register from "./components/register/register";
 import store from "./redux/store";
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <Register />
       </Provider>
       ,
     </>
