dc87a6aa3abaf66ba9f267662da1546a60107989
galoss88 <gallogarcianicolas@gmail.com>
2023-08-16 00:52:37 -0300

    quitado componente de app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,10 @@
 import { Provider } from "react-redux";
-import Register from "./components/register/register";
 import store from "./redux/store";
 
 function App() {
   return (
     <>
       <Provider store={store}>
-        <Register />
       </Provider>
       ,
     </>
