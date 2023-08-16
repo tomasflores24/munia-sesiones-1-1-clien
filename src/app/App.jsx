3fb42ecf13ec6f4be7e73f2a0511e5c0bdba255d
Pedro Terraf <pedroterraf@gmail.com>
2023-08-16 15:53:41 -0300

    Testeo

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,16 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Register from "./components/register/register";
+import Table from "./components/Table/Table";
 import { BrowserRouter } from "react-router-dom";
+
 function App() {
   return (
     <>
       <BrowserRouter>
         <Provider store={store}>
-          <Register />
+            <Register />
+            <Table />
         </Provider>
       </BrowserRouter>
       ,
