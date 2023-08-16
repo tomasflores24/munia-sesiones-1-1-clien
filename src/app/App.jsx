3389bb3bc17e836a55cc85f6c2a463e419c8937d
galoss88 <gallogarcianicolas@gmail.com>
2023-08-16 14:42:09 -0300

    agregado estructura de carpeta de las rutas

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,13 +1,15 @@
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Register from "./components/register/register";
-
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <>
-      <Provider store={store}>
-        <Register/>
-      </Provider>
+      <BrowserRouter>
+        <Provider store={store}>
+          <Register />
+        </Provider>
+      </BrowserRouter>
       ,
     </>
   );
---
+++ src/app/routes/index.js
@@ -0,0 +1,14 @@
+import { Routes } from "react-router-dom";
+// Importa más páginas si es necesario
+
+function AppRoutes() {
+  return (
+    <Routes>
+      {/* ejemplo */}
+      {/* <Route path="/" element={<HomePage />} /> */}
+      {/* Agregar más rutas aca */}
+    </Routes>
+  );
+}
+
+export default AppRoutes;
