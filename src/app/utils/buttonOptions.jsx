5463e287c98e85604eb0eb6ee658f3f6337d93a8
EugeniaGramajo <gramajoeugenia@gmail.com>
2023-08-17 15:23:29 -0300

    update(stile buttons siodeBar and routes)

--- src/app/utils/buttonOptions.jsx
+++ src/app/utils/buttonOptions.jsx
@@ -1,11 +1,11 @@
 import HomeIcon from '@mui/icons-material/Home';
 import PersonIcon from '@mui/icons-material/Person';
-import TodayIcon from '@mui/icons-material/Today';
+import EventNoteIcon from '@mui/icons-material/EventNote';
 import SettingsIcon from '@mui/icons-material/Settings';
 
 export const buttonData = [
     { title: "Home", icon: <HomeIcon style={{ fontSize: '30px' }} />, redirect: "/home" },
     { title: "Perfil", icon: <PersonIcon style={{ fontSize: '30px' }} />, redirect: "/perfil" },
-    { title: "Agenda", icon: <TodayIcon style={{ fontSize: '30px' }} />, redirect: "/agenda" },
+    { title: "Agenda", icon: <EventNoteIcon style={{ fontSize: '30px' }} />, redirect: "/agenda" },
     { title: "Configuración", icon: <SettingsIcon style={{ fontSize: '30px' }} />, redirect: "/configuracion" },
 ];
