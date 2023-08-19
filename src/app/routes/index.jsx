1d29c6a9de47aab0a1254d2864a5c6650a9430d6
Pedro Terraf <pedroterraf@gmail.com>
2023-08-19 03:40:20 -0300

    Feature/Home

--- src/app/routes/index.jsx
+++ src/app/routes/index.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Register from "../components/register/register";
 import Table from "../components/Table/Table";
 import Sidebar from "../components/sideBar/SideBar"
+import Home from "../views/Home/Home";
 // Importa más páginas si es necesario
 
 function AppRoutes() {
@@ -11,6 +12,7 @@ function AppRoutes() {
       <Route path="/user/register" element={<Register />} />
       <Route path="/table" element={<Table />} />
       <Route path="/sidebar" element={<Sidebar />} />
+      <Route path="/home" element={<Home />} />
       
       {/* Agregar más rutas aca */}
     </Routes>
