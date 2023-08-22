5c8c04438b518d2c02993ff68737f44f6c7d77b5
Pedro Terraf <pedroterraf@gmail.com>
2023-08-22 03:14:22 -0300

    pull changes

--- src/app/views/Home/Home.jsx
+++ src/app/views/Home/Home.jsx
@@ -1,166 +1,29 @@
 import SideBar from '../../components/sideBar/SideBar';
+import Card from '../../components/CardsHome/Cards';
+import EventNoteIcon from '@mui/icons-material/EventNote';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import PeopleIcon from '@mui/icons-material/People';
+import TextsmsIcon from '@mui/icons-material/Textsms';
+import PsychologyIcon from '@mui/icons-material/Psychology';
+import SettingsIcon from '@mui/icons-material/Settings';
 import './Home.scss'
 
 const Home = () => {
-
-
-    return (
-        <div className='HomeContainer'>
-            <SideBar />
-            <div className='ContainAll'>
-                <div className='containerTop'>
-                    <div className='container1Top'>
-                        <div className='titleTop'>
-                            <p>Promedio de sesiones por psicólogo/paciente.</p>
-                        </div>
-                        <div className='BoxTop'>
-                            <p>5</p>
-                        </div>
-                    </div>
-                    <div className='container1Top'>
-                        <div className='titleTop'>
-                            <p>sesiones programadas y realizadas en el año</p>
-                        </div>
-                        <div className='BoxTop'>
-                            <p>547</p>
-                        </div>
-                    </div>
-                    <div className='container1Top'>
-                        <div className='titleTop'>
-                            <p>Total de cancelación y reprogramación de citas.</p>
-                        </div>
-                        <div className='BoxTop'>
-                            <p>113</p>
-                        </div>
-                    </div>
-                    <div className='container1Top'>
-                        <div className='titleTop'>
-                            <p>Cantidad de psicólogos registrados en la plataforma.</p>
-                        </div>
-                        <div className='BoxTop'>
-                            <p>15</p>
-                        </div>
-                    </div>
-                </div>
-                <div className='containerMid'>
-                    <div className='BoxMid1'>
-                        <div className='titleMid1'>
-                            <p>Resumen de comentarios y opiniones de los pacientes</p>
-                        </div>
-                        <div className='BoxCards1'>
-                            <div className='Cards1'>
-                                <div className='CardsTitle1'>
-                                    <p>Juan Pablo Osudar - 17/8/23</p>
-                                </div>
-                                <div className='CardsDesc1'>
-                                    <div className='BoxDescText1'>
-                                        <p>Estoy realmente satisfecho con las sesiones. El enfoque comprensivo y las estrategias proporcionadas han mejorado mi perspectiva y bienestar.</p>
-                                    </div>
-                                    <div className='BoxDescImg1'>
-                                        STAR
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='BoxCards1'>
-                            <div className='Cards1'>
-                                <div className='CardsTitle1'>
-                                    <p>Joel Ochoa - 17/8/23</p>
-                                </div>
-                                <div className='CardsDesc1'>
-                                    <div className='BoxDescText1'>
-                                        <p>Mi terapeuta ha sido un apoyo invaluable en mi viaje emocional. Sus enfoques personalizados y empatía genuina han marcado una diferencia positiva en mi bienestar mental.</p>
-                                    </div>
-                                    <div className='BoxDescImg1'>
-                                        STAR
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className='BoxCards1'>
-                            <div className='Cards1'>
-                                <div className='CardsTitle1'>
-                                    <p>Juan Camilo Moreno - 16/8/23</p>
-                                </div>
-                                <div className='CardsDesc1'>
-                                    <div className='BoxDescText1'>
-                                        <p>No encontré la conexión que esperaba con mi terapeuta. Las sesiones a menudo se sintieron superficiales y carecían de dirección en mi proceso de sanación.</p>
-                                    </div>
-                                    <div className='BoxDescImg1'>
-                                        STAR
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='BoxMid2'>
-                        <div className='ContainerMid2'>
-                            <div className='titleMid2'>
-                                <p>Citas recientes programadas</p>
-                            </div>
-                            <div className='BoxCards2'>
-                                <div className='CardsContainer'>
-                                    <div className='Cards2'>
-                                        <p>Pedro Terraf</p>
-                                        <div>
-                                            <p>Mie. 16 - 08 - 2023</p>
-                                            <p>13:30 pm</p>
-                                        </div>
-                                        <p>Rafael Santandreu</p>
-                                    </div>
-                                    <div className='Cards2'>
-                                        <p>Juan Pablo Osudar</p>
-                                        <div>
-                                            <p>Mie. 16 - 08 - 2023</p>
-                                            <p>13:30 pm</p>
-                                        </div>
-                                        <p>María Agustina Lahitou</p>
-                                    </div>
-                                    <div className='Cards2'>
-                                        <p>Juan Camilo Moreno</p>
-                                        <div>
-                                            <p>Mie. 16 - 08 - 2023</p>
-                                            <p>10:30 am</p>
-                                        </div>
-                                        <p>Rafael Santandreu</p>
-                                    </div>
-                                    <div>
-                                        <p>Joel Ochoa</p>
-                                        <div>
-                                            <p>Mie. 16 - 08 - 2023</p>
-                                            <p>10:30 am</p>
-                                        </div>
-                                        <p>María Agustina Lahitou</p>
-                                    </div>
-                                    <div>
-                                        <button>Ver todas las citas programadas</button>
-                                    </div>
-                                </div>
-                                <div>
-                                    <div>
-                                        <div>
-                                            <p>Total de citas programadas para hoy.</p>
-                                        </div>
-                                        <div>
-                                            <p>10</p>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div>
-                                            <p>Total de citas canceladas hoy.</p>
-                                        </div>
-                                        <div>
-                                            <p>5</p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className='AllHome'>
+      <SideBar />
+      <div className='HomeContainer'>
+        <div className='ContainerCards'>
+          <Card to='/admin/agenda' imgComponent={<EventNoteIcon className='Img' />} title='Agenda' variables='.-' />
+          <Card to='/admin/statistics' imgComponent={<AnalyticsIcon className='Img' />} title='Estadísticas' variables='.-' />
+          <Card to='/admin/clients' imgComponent={<PeopleIcon className='Img' />} title='Clientes' variables='37' />
+          <Card to='/admin/comments' imgComponent={<TextsmsIcon className='Img' />} title='Comentarios' variables='1130' />
+          <Card to='/admin/providers' imgComponent={<PsychologyIcon className='Img' />} title='Proveedores' variables='5' />
+          <Card to='/admin/memberships' imgComponent={<SettingsIcon className='Img' />} title='Membresías' variables='.-' />
         </div>
-    )    
-}
+      </div>
+    </div>
+  );
+};
 
 export default Home;
