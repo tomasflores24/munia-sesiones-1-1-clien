7757caf66b3e564d047b1efaa298dc826bbe8daf
Pedro Terraf <pedroterraf@gmail.com>
2023-08-28 20:30:25 -0300

    push login

--- src/app/pages/auth/sign-in/SignIn.jsx
+++ src/app/pages/auth/sign-in/SignIn.jsx
@@ -1,17 +1,30 @@
 import "./SignInStyle.scss";
-import { TextField } from "@mui/material";
+import * as React from 'react';
+import { Button, TextField } from "@mui/material";
 import { useMemo } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import IconButton from '@mui/material/IconButton';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 import * as yup from "yup";
 
 const SignIn = () => {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+
   const validationSchema = useMemo(() => {
     return yup.object({
       email: yup
         .string()
         .required("Correo es requerido")
         .matches(
+          // eslint-disable-next-line no-useless-escape
           /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
           "Ingrese una dirección de correo electrónico válida"
         ),
@@ -21,6 +34,10 @@ const SignIn = () => {
         .min(5, "La contraseña tiene que ser de minimo 5 caracteres"),
     });
   }, []);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
 
   const {
     formState: { errors },
@@ -42,15 +59,46 @@ const SignIn = () => {
         className="login-img"
         alt=""
       />
-      <h1>Ingresar</h1>
       <form className="form" onSubmit={handleSubmit(customHandleSubmit)}>
-        <TextField {...register("email")} helperText={errors?.email?.message} />
-        <TextField
-          {...register("password")}
-          type="password"
-          helperText={errors?.password?.message}
-        />
-        <button type="submit">Enviar</button>
+        <div className="titleH1">
+          <h1>Ingresar</h1>
+        </div>
+        <div className="formBox">
+          <FormControl sx={{ m: 1, width: '40ch' }} variant="outlined">
+          <h2>Correo</h2>
+            <TextField
+              {...register("email")} 
+              helperText={errors?.email?.message}
+              label="Usuario"
+              id="filled-password-input"
+              variant="standard"
+            />
+            <h2>Contraseña</h2>
+              <TextField
+                id="filled-password-input"
+                label="Password"
+                autoComplete="current-password"
+                variant="standard"
+                {...register("password")}
+                helperText={errors?.password?.message}
+                type={showPassword ? 'text' : 'password'}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            <p className="ResetPassword">¿Has olvidado tu contraseña?</p>
+            <Button type="submit">Enviar</Button>
+          </FormControl>
+        </div>
       </form>
     </div>
   );
