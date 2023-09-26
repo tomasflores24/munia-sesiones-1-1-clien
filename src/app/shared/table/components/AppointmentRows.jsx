b63ea5cfd6af57298677561f6f3f0769945c7b2c
DanielRicra <daniel.int@outlook.com>
2023-09-25 20:48:44 -0500

    Se completo AppointmentRows
    Se agrego las rows con datos por defecto

--- src/app/shared/table/components/AppointmentRows.jsx
+++ src/app/shared/table/components/AppointmentRows.jsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import PropTypes from 'prop-types'
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import { Chip } from '@mui/material';
 
-const AppointmentRows = () => {
+const AppointmentRows = ({ data, onEdit, onDelete }) => {
   return (
     <div>
-      
+      <div className="shared_table-body">
+        {data.map((appointment, index) => (
+          <div className="row-table-container" key={index}>
+            <div className="data-row-tag">
+              <img src="" alt="picture" />
+              <p>Name</p>
+            </div>
+            <div className="data-row-tag">
+              Mie. 16-08-2023
+              <br />
+              13:30pm
+            </div>
+            <div className="data-row-tag">
+              <Chip label="Nutricion" size='small' className='service-type' />              
+            </div>
+            <div className="data-row-tag">
+              Confirmado  <CheckCircleOutlineIcon fontSize='22' stroke='#068F1C' />
+            </div>
+            <div className="data-row-tag">Maria@munia.com</div>
+            <div className="data-row-tag data-row-actions">
+              <button
+                type='button'
+                onClick={onEdit}
+                className='action-button'
+              >
+                Editar
+              </button>
+              <button
+                type='button'
+                onClick={onDelete}
+                className='action-button'
+              >
+                Eliminar
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
+}
+
+AppointmentRows.propTypes = {
+  data: PropTypes.array,
+  onEdit: PropTypes.func,
+  onDelete: PropTypes.func,
 }
 
 export default AppointmentRows
