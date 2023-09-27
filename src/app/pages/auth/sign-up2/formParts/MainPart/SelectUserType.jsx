93a7379424516ba1849213d669e8e60b85165dcc
DanielRicra <daniel.int@outlook.com>
2023-09-27 08:39:28 -0500

    Se agrego el paso principal en el registro
    El paso prinsipal consiste en seleccionar si es usuario o psicologo, segun esto se muestran los pasos.

---
+++ src/app/pages/auth/sign-up2/formParts/MainPart/SelectUserType.jsx
@@ -0,0 +1,63 @@
+import PropTypes from "prop-types";
+
+import "./SelectUserTypeStyle.scss";
+import { UserTypes } from "../../../../../utils/registerUserType";
+
+const SelectUserType = ({ setStep, setUserType, userType }) => {
+  const handleClick = (type) => {
+    setUserType(type);
+  };
+
+  return (
+    <section className="selectUserType__root">
+      <div className="selectUserType__topWrapper">
+        <h2 className="selectUserType__title">
+          Selecciona tu tipo de usuario.
+        </h2>
+
+        <div className="user_type_buttons">
+          <button
+            className={`user ${userType === UserTypes.user ? "selected" : ""}`}
+            onClick={() => handleClick(UserTypes.user)}
+          >
+            Usuario
+          </button>
+          <button
+            className={`psychologist  ${
+              userType === UserTypes.psychologist ? "selected" : ""
+            }`}
+            onClick={() => handleClick(UserTypes.psychologist)}
+          >
+            Psicólogo
+          </button>
+        </div>
+      </div>
+
+      <div className="actions">
+        <button type="button" className="back_btn disabled">
+          atras
+        </button>
+        <p className="actions__text_slogan">
+          Nuestra vision es tu <span>bienestar</span>
+        </p>
+        <button
+          type="button"
+          onClick={() => setStep((prev) => prev + 1)}
+          className={
+            userType !== UserTypes.none ? "submit_btn" : "submit_btn disabled"
+          }
+        >
+          Siguiente
+        </button>
+      </div>
+    </section>
+  );
+};
+
+SelectUserType.propTypes = {
+  setStep: PropTypes.func,
+  setUserType: PropTypes.any,
+  userType: PropTypes.number,
+};
+
+export default SelectUserType;
---
+++ src/app/utils/registerUserType.js
@@ -0,0 +1,5 @@
+export const UserTypes = {
+  none: 0,
+  user: 1,
+  psychologist: 2,
+};
