396e9882c7a8ecc84f7f7a39036888dd6577a69f
DanielRicra <daniel.int@outlook.com>
2023-10-04 11:42:06 -0500

    Se arego useGetProviderServices hook

--- src/app/hooks/Register/useServices.js
+++ src/app/hooks/Register/useServices.js
@@ -4,3 +4,9 @@ import { ServiceServices } from "../../services/auth/service.services";
 export function useGetServices() {
   return useQuery(["services"], ServiceServices.getServices);
 }
+
+export function useGetProviderServices({ providerId }) {
+  return useQuery(["providerServices", providerId], () =>
+    ServiceServices.getProviderServices(providerId)
+  );
+}
