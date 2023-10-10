5b57c336fb2e8f1b5e4955db6456ccf8573ce731
DanielRicra <daniel.int@outlook.com>
2023-10-10 11:28:00 -0500

    Se actualizo calendar.js
    - Se añadio la funcion `groupDataByDay`.
    -Se añadio array de eses y dias de la semana.

--- src/app/utils/calendar.js
+++ src/app/utils/calendar.js
@@ -1,4 +1,4 @@
-import { add, format, getDate, getYear, nextMonday } from "date-fns";
+import { add, format, getDate, getYear, nextMonday, getWeek } from "date-fns";
 
 export function calculateWeekMinMaxDates() {
   const today = new Date();
@@ -25,9 +25,50 @@ export function calculateMonthMinDate() {
   const month = canSelectCurrentMonth
     ? format(today, "MM")
     : format(nextMonth, "MM");
-    
+
   const year = canSelectCurrentMonth ? getYear(today) : getYear(nextMonth);
   return {
     min: `${year}-${month}`,
   };
+}
+
+export const daysOfWeek = ["Dom", "Lun", "Mar", "Mié", "Jue", "Vie", "Sáb"];
+
+export const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Setiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+
+export function groupDataByDay(data) {
+  const groupedData = new Array(7).fill(null);
+  const firstWeekNumber = getWeek(new Date(data[0].startTime.slice(0, -1)));
+
+  for (let i = 0; i < data.length; i++) {
+    const item = data[i];
+
+    const startTime = new Date(item.startTime.slice(0, -1));
+    const currentWeekNumber = getWeek(startTime);
+
+    if (currentWeekNumber !== firstWeekNumber) {
+      continue;
+    }
+    const dayOfWeek = startTime.getDay(); // Get the day of the week
+
+    if (groupedData[dayOfWeek] === null) {
+      groupedData[dayOfWeek] = [];
+    }
+    groupedData[dayOfWeek].push(item);
+  }
+
+  return groupedData;
 }
