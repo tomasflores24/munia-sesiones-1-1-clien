3f43429339ded62373f2ac51ffe28dcfafbc3f94
DanielRicra <daniel.int@outlook.com>
2023-10-11 17:09:22 -0500

    Datos de referencia, trabajo en equipo

--- src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
+++ src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
@@ -1,12 +1,33 @@
+import { useState } from "react";
 import { Toaster } from "react-hot-toast";
 
-import AppointmentCalendar from "../../../components/AppointmentCalendar/AppointmentCalendar";
+// import AppointmentCalendar from "../../../components/AppointmentCalendar/AppointmentCalendar";
 
 const CreateAppointment = () => {
+  // const [selectedCategory, setSelectedCategory] = useState(null);
+  // const [selectedService, setSelectedService] = useState(null);
+  // const [selectedProvider, setSelectedProvider] = useState(null);
+
+  // use mutattion
+
   return (
     <div style={{ marginLeft: "50px" }}>
       <div>CreateAppointment</div>
-      <AppointmentCalendar />
+      {/* <Category setSelectedCategory={setSelectedCategory} />
+      {selectedCategory && (
+        <Service
+          selectedCategory={selectedCategory}
+          setSelectedService={setSelectedService}
+        />
+      )}
+      {selectedService && (
+        <Provider
+          setSelectedProvider={setSelectedProvider}
+          selectedService={selectedService}
+        />
+      )}
+      {selectedProvider && <AppointmentCalendar />} */}
+
       <Toaster position="bottom-center" />
     </div>
   );
