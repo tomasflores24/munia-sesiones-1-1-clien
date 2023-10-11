51dad6c5099e9ae9653e1afbbad983ca9f6645bc
FrancoDodera <francododera@hotmail.com>
2023-10-11 17:37:17 -0300

    Vista Citas
    se implementaron las citas

--- src/app/services/dashboard/appointments/appointment.service.js
+++ src/app/services/dashboard/appointments/appointment.service.js
@@ -1,7 +1,18 @@
 import { HttpRequest } from "../../HttpRequest";
 
 export const AppointmentService = {
-  getAllAppointments: async () => HttpRequest.get("/collaborator"),
   updateAppointment: async ({ providerId, body }) =>
     HttpRequest.patch(`/appointment/${providerId}`, body),
+
+  getAppoiments: async (CollaboratorId) =>
+    HttpRequest.get(
+      CollaboratorId
+        ? `/appointment/d104e405-9329-4fc5-89f6-c8eeb9f51650?CollaboratorId=${CollaboratorId}`
+        : "/appointment/d104e405-9329-4fc5-89f6-c8eeb9f51650"
+    ),
+
+  cancelAppointment: async (id) =>
+    HttpRequest.get(`/appointment/cancelAppointment/${id}`),
+
+  // getAppoiments: async () => HttpRequest.get("/appointment/appointments"),
 };
