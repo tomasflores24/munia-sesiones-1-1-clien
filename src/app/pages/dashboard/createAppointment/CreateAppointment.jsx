267ff74ea4830ca491f89b9ed0f5f968ff0dfc4f
DanielRicra <daniel.int@outlook.com>
2023-10-12 11:34:57 -0500

    Estilos terminados

--- src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
+++ src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
@@ -43,47 +43,55 @@ const CreateAppointment = () => {
   return (
     <>
       <div className="createAppointment__root">
-        <h1 className="title">Agendar tu cita</h1>
-        <SelectCategory
-          selectedCategoryId={selectedCategoryId}
-          setSelectedCategoryId={setSelectedCategoryId}
-        />
-        {selectedCategoryId === "" ? null : (
-          <SelectService
-            categoryId={selectedCategoryId}
-            setSelectedServiceId={setSelectedServiceId}
-            selectedService={selectedServiceId}
+        <div className="createAppointment__container">
+          <div className="createAppointment__header">
+            <h1 className="title">Agendar tu cita</h1>
+            <p className="desc">Sesiones restantes 15</p>
+          </div>
+          <SelectCategory
+            selectedCategoryId={selectedCategoryId}
+            setSelectedCategoryId={setSelectedCategoryId}
           />
-        )}
+          {selectedCategoryId === "" ? null : (
+            <SelectService
+              categoryId={selectedCategoryId}
+              setSelectedServiceId={setSelectedServiceId}
+              selectedService={selectedServiceId}
+            />
+          )}
 
-        <div className="textArea-container">
-          <label className="textArea-label">Razón de la consulta</label>
-          <textarea className="textArea" rows="4" />
+          <div className="textArea-container">
+            <label className="textArea-label">Razón de la consulta</label>
+            <textarea className="textArea" rows="4" />
+          </div>
+
+          {selectedProviderId ? (
+            <AppointmentCalendar
+              providerId={selectedProviderId}
+              selectedHour={selectedHour}
+              setSelectedHour={setSelectedHour}
+            />
+          ) : null}
+
+          <div className="createAppointment__footer">
+            <h3 className="providerSelected">Profesional Seleccionado</h3>
+            {selectedServiceId ? (
+              <SelectProvider
+                setSelectedProviderId={setSelectedProviderId}
+                serviceId={selectedServiceId}
+              />
+            ) : null}
+
+            <button
+              className="confirm-button"
+              disabled={isLoading}
+              type="button"
+              onClick={onSubmit}
+            >
+              Confirmar
+            </button>
+          </div>
         </div>
-
-        {selectedServiceId ? (
-          <SelectProvider
-            setSelectedProviderId={setSelectedProviderId}
-            serviceId={selectedServiceId}
-          />
-        ) : null}
-
-        {selectedProviderId ? (
-          <AppointmentCalendar
-            providerId={selectedProviderId}
-            selectedHour={selectedHour}
-            setSelectedHour={setSelectedHour}
-          />
-        ) : null}
-
-        <button
-          className="confirm-button"
-          disabled={isLoading}
-          type="button"
-          onClick={onSubmit}
-        >
-          Confirmar
-        </button>
       </div>
       <Toaster position="bottom-center" />
     </>
