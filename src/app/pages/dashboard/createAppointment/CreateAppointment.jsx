91f6ebdd5d90e2885065ead3d8d858f15fa88f7b
DanielRicra <daniel.int@outlook.com>
2023-10-12 09:28:11 -0500

    Se agrego el onSubmit

--- src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
+++ src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
@@ -1,47 +1,92 @@
 import { useState } from "react";
-import { Toaster } from "react-hot-toast";
+import toast, { Toaster } from "react-hot-toast";
 import SelectCategory from "./components/selectCategory/SelectCategory";
 import SelectService from "./components/selectService/selectService";
 import "./CreateAppointment.style.scss";
 
 import AppointmentCalendar from "../../../components/AppointmentCalendar/AppointmentCalendar";
 import SelectProvider from "./components/provider/SelectProvider";
+import { useMutation, useQueryClient } from "react-query";
+import { AppointmentService } from "../../../services/dashboard/appointments/appointment.service";
 
 const CreateAppointment = () => {
   const [selectedCategoryId, setSelectedCategoryId] = useState("");
   const [selectedServiceId, setSelectedServiceId] = useState(null);
   const [selectedProviderId, setSelectedProviderId] = useState(null);
+  const [selectedHour, setSelectedHour] = useState({ id: undefined });
+
+  const queryClient = useQueryClient();
+
+  const { mutate, isLoading } = useMutation(
+    AppointmentService.createAppointment,
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("providerAvailable");
+        toast.success("Reunión agendada con éxito");
+        setSelectedHour({ id: undefined });
+      },
+      onError: (err) => {
+        toast.error(err.response?.data?.error || "Algo salio mal");
+      },
+    }
+  );
+  console.log({ selectedProviderId, selectedHour });
+
+  const onSubmit = () => {
+    mutate({
+      ServiceId: selectedServiceId,
+      CollaboratorId: "f4c2d071-b2a9-4c71-b1ea-96bfc2d4b19e",
+      ProviderId: selectedHour.ProviderId,
+      AvailableId: selectedHour.id,
+    });
+  };
 
   return (
-    <div className="createAppointment__root">
-      <SelectCategory
-        selectedCategoryId={selectedCategoryId}
-        setSelectedCategoryId={setSelectedCategoryId}
-      />
-      {selectedCategoryId === "" ? null : (
-        <SelectService
-          categoryId={selectedCategoryId}
-          setSelectedServiceId={setSelectedServiceId}
-          selectedService={selectedServiceId}
+    <>
+      <div className="createAppointment__root">
+        <SelectCategory
+          selectedCategoryId={selectedCategoryId}
+          setSelectedCategoryId={setSelectedCategoryId}
         />
-      )}
+        {selectedCategoryId === "" ? null : (
+          <SelectService
+            categoryId={selectedCategoryId}
+            setSelectedServiceId={setSelectedServiceId}
+            selectedService={selectedServiceId}
+          />
+        )}
 
-      <div className="textArea-container">
-        <label className="textArea-label">Razón de la consulta</label>
-        <textarea className="textArea" rows="4"></textarea>
+        <div className="textArea-container">
+          <label className="textArea-label">Razón de la consulta</label>
+          <textarea className="textArea" rows="4" />
+        </div>
+
+        {selectedServiceId ? (
+          <SelectProvider
+            setSelectedProviderId={setSelectedProviderId}
+            serviceId={selectedServiceId}
+          />
+        ) : null}
+
+        {selectedProviderId ? (
+          <AppointmentCalendar
+            providerId={selectedProviderId}
+            selectedHour={selectedHour}
+            setSelectedHour={setSelectedHour}
+          />
+        ) : null}
+
+        <button
+          className="confirm-button"
+          disabled={isLoading}
+          type="button"
+          onClick={onSubmit}
+        >
+          Confirm
+        </button>
       </div>
-
-      {selectedServiceId ? (
-        <SelectProvider
-          setSelectedProviderId={setSelectedProviderId}
-          serviceId={selectedServiceId}
-        />
-      ) : null}
-
-      {selectedProviderId ? <AppointmentCalendar /> : null}
-
       <Toaster position="bottom-center" />
-    </div>
+    </>
   );
 };
 export default CreateAppointment;
