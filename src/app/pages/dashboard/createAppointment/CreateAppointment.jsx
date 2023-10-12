e4056c15c8c50f29850797c0b847dea3b0415904
FrancoDodera <francododera@hotmail.com>
2023-10-12 15:16:45 -0300

    Loading spiner
    se agrego loading spiner al terminar el agendamiento

--- src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
+++ src/app/pages/dashboard/createAppointment/CreateAppointment.jsx
@@ -8,6 +8,7 @@ import SelectCategory from "./components/selectCategory/SelectCategory";
 import AppointmentCalendar from "../../../components/AppointmentCalendar/AppointmentCalendar";
 import SelectProvider from "./components/provider/SelectProvider";
 import { AppointmentService } from "../../../services/dashboard/appointments/appointment.service";
+import LoadingSpinner from "../../../shared/loadingSpinner/LoadingSpinner";
 
 const CreateAppointment = () => {
   const [selectedCategoryId, setSelectedCategoryId] = useState("");
@@ -24,6 +25,9 @@ const CreateAppointment = () => {
         queryClient.invalidateQueries("providerAvailable");
         toast.success("Reunión agendada con éxito");
         setSelectedHour({ id: undefined });
+        setSelectedCategoryId("");
+        setSelectedServiceId(null);
+        setSelectedProviderId(null);
       },
       onError: (err) => {
         toast.error(err.response?.data?.error || "Algo salio mal");
@@ -43,55 +47,59 @@ const CreateAppointment = () => {
   return (
     <>
       <div className="createAppointment__root">
-        <div className="createAppointment__container">
-          <div className="createAppointment__header">
-            <h1 className="title">Agendar tu cita</h1>
-            <p className="desc">Sesiones restantes 15</p>
-          </div>
-          <SelectCategory
-            selectedCategoryId={selectedCategoryId}
-            setSelectedCategoryId={setSelectedCategoryId}
-          />
-          {selectedCategoryId === "" ? null : (
-            <SelectService
-              categoryId={selectedCategoryId}
-              setSelectedServiceId={setSelectedServiceId}
-              selectedService={selectedServiceId}
+        {isLoading ? (
+          <LoadingSpinner />
+        ) : (
+          <div className="createAppointment__container">
+            <div className="createAppointment__header">
+              <h1 className="title">Agendar tu cita</h1>
+              <p className="desc">Sesiones restantes 15</p>
+            </div>
+            <SelectCategory
+              selectedCategoryId={selectedCategoryId}
+              setSelectedCategoryId={setSelectedCategoryId}
             />
-          )}
+            {selectedCategoryId === "" ? null : (
+              <SelectService
+                categoryId={selectedCategoryId}
+                setSelectedServiceId={setSelectedServiceId}
+                selectedService={selectedServiceId}
+              />
+            )}
 
-          <div className="textArea-container">
-            <label className="textArea-label">Razón de la consulta</label>
-            <textarea className="textArea" rows="4" />
-          </div>
+            <div className="textArea-container">
+              <label className="textArea-label">Razón de la consulta</label>
+              <textarea className="textArea" rows="4" />
+            </div>
 
-          {selectedProviderId ? (
-            <AppointmentCalendar
-              providerId={selectedProviderId}
-              selectedHour={selectedHour}
-              setSelectedHour={setSelectedHour}
-            />
-          ) : null}
-
-          <div className="createAppointment__footer">
-            <h3 className="providerSelected">Profesional Seleccionado</h3>
-            {selectedServiceId ? (
-              <SelectProvider
-                setSelectedProviderId={setSelectedProviderId}
-                serviceId={selectedServiceId}
+            {selectedProviderId ? (
+              <AppointmentCalendar
+                providerId={selectedProviderId}
+                selectedHour={selectedHour}
+                setSelectedHour={setSelectedHour}
               />
             ) : null}
 
-            <button
-              className="confirm-button"
-              disabled={isLoading}
-              type="button"
-              onClick={onSubmit}
-            >
-              Confirmar
-            </button>
+            <div className="createAppointment__footer">
+              <h3 className="providerSelected">Profesional Seleccionado</h3>
+              {selectedServiceId ? (
+                <SelectProvider
+                  setSelectedProviderId={setSelectedProviderId}
+                  serviceId={selectedServiceId}
+                />
+              ) : null}
+
+              <button
+                className="confirm-button"
+                disabled={isLoading}
+                type="button"
+                onClick={onSubmit}
+              >
+                Confirmar
+              </button>
+            </div>
           </div>
-        </div>
+        )}
       </div>
       <Toaster position="bottom-center" />
     </>
