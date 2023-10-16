d183d4b1a5dc45382d1b9a40c377aeca86a99827
DanielRicra <daniel.int@outlook.com>
2023-10-16 15:07:16 -0500

    Mover estilos en linea a scss

--- src/app/components/sideBar/SideBar.jsx
+++ src/app/components/sideBar/SideBar.jsx
@@ -4,33 +4,23 @@ import { Drawer, IconButton } from "@mui/material";
 import "./SideBar.scss";
 import { useSelector } from "react-redux";
 import NavItems from "./commonSideBar/NavItems";
-import muniaLogo from '/assets/muniaLogo.png'
+import muniaLogo from "/assets/muniaLogo.png";
 
 const ADMIN = 4;
 
 export default function SideBar() {
   const [openSidebar, setOpenSidebar] = useState(false);
-  const user = useSelector((state) => state.auth.auth.user)
+  const user = useSelector((state) => state.auth.auth.user);
 
   const toggleSidebar = () => {
     setOpenSidebar(!openSidebar);
-  };  
-
+  };
 
   return (
     <>
       <IconButton
         className="menuButton"
         onClick={toggleSidebar}
-        sx={{
-          display: {
-            xs: "block",
-            sm: "block",
-            md: "block",
-            lg: "none",
-            xl: "none",
-          },
-        }}
       >
         <MenuIcon />
       </IconButton>
@@ -38,19 +28,11 @@ export default function SideBar() {
         <nav className="container-responsive">
           {user.userTypeId === ADMIN ? (
             <header className="admin-data">
-              <img
-                src={muniaLogo}
-                className="img"
-                alt="Logo"
-              />
+              <img src={muniaLogo} className="img" alt="Logo" />
             </header>
           ) : (
             <header className="profile-data">
-              <img
-                src={user.profilePic}
-                className="img"
-                alt="Profile"
-              />
+              <img src={user.profilePic} className="img" alt="Profile" />
               <h2 className="user-data">María Agustina Lahitou</h2>
             </header>
           )}
@@ -62,11 +44,7 @@ export default function SideBar() {
       <nav className="container">
         {user.userTypeId === ADMIN ? (
           <header className="admin-data">
-            <img
-              src={muniaLogo}
-              className="img"
-              alt="Logo"
-            />
+            <img src={muniaLogo} className="img" alt="Logo" />
           </header>
         ) : (
           <header className="profile-data">
