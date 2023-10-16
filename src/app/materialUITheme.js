da0c78c27342ae026acc606d0288ca9e9b5a84f5
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2023-10-16 18:47:43 -0300

    Tipografía de los componentes de MUI Montserrat

--- src/app/materialUITheme.js
+++ src/app/materialUITheme.js
@@ -1,12 +1,16 @@
 import { createTheme } from "@mui/material";
 
 export const appTheme = createTheme({
+    typography: {
+        fontFamily: "Montserrat, sans-serif",
+        fontSize: "100px"
+    },
     palette: {
         primary: {
             main: '#ae7a6c'
         },
         secondary: {
-            main: '#ebdcc1',
+            main: '#ebdcc1'
         },
         tertiary: {
             main: '#535353'
