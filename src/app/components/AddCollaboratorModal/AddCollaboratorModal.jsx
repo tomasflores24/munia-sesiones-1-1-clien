e5f8c20287300454f4c2936d90293058108d5eb9
FrancoDodera <francododera@hotmail.com>
2023-10-16 13:57:45 -0300

    se arreglaron los servicios
    se arreglaro el collaborators.service y se implemento funcionalidad en AddCollaboratorModal

--- src/app/components/AddCollaboratorModal/AddCollaboratorModal.jsx
+++ src/app/components/AddCollaboratorModal/AddCollaboratorModal.jsx
@@ -12,7 +12,9 @@ import {
   Input,
   InputAdornment,
   InputLabel,
+  Select,
   TextField,
+  MenuItem,
 } from "@mui/material";
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -37,6 +39,7 @@ const collaboratorSchema = yup.object({
       "Ingresa un correo valido"
     ),
   phone: yup.string().required("El telefono es requerido"),
+  country: yup.string().required("El pais es requerido"),
   password: yup
     .string()
     .required("La contraseña es requerida")
@@ -74,20 +77,23 @@ const AddCollaboratorModal = ({ handleModal, isLoading }) => {
     CollaboratorsService.createCollaborator(data)
   );
 
+  const { data: countries } = useQuery(["getAllCountries"], () =>
+    CollaboratorsService.getAllCountries()
+  );
   const onSubmit = async (data) => {
     // collaboratorMutation.mutate(data);
     console.log(data);
     const res = await mutateAsync({
-      profilePic: register.profilePic,
-      name: register.name,
-      lastName: register.lastName,
-      email: register.email,
-      password: register.password,
+      profilePic: data.profilePic,
+      name: data.name,
+      lastName: data.lastName,
+      email: data.email,
+      password: data.password,
       CountryId: 1,
       city: "Cityville",
-      isActive: false,
+      isActive: true,
       UserTypeId: 2,
-      GenderId: 3,
+      GenderId: data.genderID,
     });
   };
 
@@ -183,6 +189,38 @@ const AddCollaboratorModal = ({ handleModal, isLoading }) => {
                 helperText={errors?.phone?.message}
                 error={!!errors?.phone}
               />
+              <Select
+                label="Pais"
+                name="CountryId"
+                variant="standard"
+                {...register("CountryId")}
+                helperText={errors?.country?.message}
+                error={!!errors?.country}
+              >
+                {countries?.data.map((el) => {
+                  return (
+                    <MenuItem key={el.id} value={el.id}>
+                      <em>{el.name}</em>
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+              <TextField
+                className="input"
+                label="City"
+                variant="standard"
+                {...register("city")}
+              />
+              <Select
+                label="Genero"
+                name="GenderId"
+                variant="standard"
+                {...register("GenderId")}
+              >
+                <MenuItem value="1">Masculino</MenuItem>
+                <MenuItem value="2">Femenino</MenuItem>
+                <MenuItem value="3">Otro</MenuItem>
+              </Select>
               <FormControl className="input" variant="standard">
                 <InputLabel htmlFor="standard-adornment-password">
                   Password
--- src/app/services/dashboard/collaborators/collaborators.service.js
+++ src/app/services/dashboard/collaborators/collaborators.service.js
@@ -2,11 +2,14 @@ import { HttpRequest } from "../../HttpRequest";
 
 export const CollaboratorsService = {
   getAllCollaborators: async () => HttpRequest.get("/collaborator"),
-  getCollaboratorById: async (collaboratorId) => HttpRequest.get(collaboratorId ? `/collaborator/${collaboratorId}` : null),
+  getCollaboratorById: async (collaboratorId) =>
+    HttpRequest.get(collaboratorId ? `/collaborator/${collaboratorId}` : null),
   updateCollaborator: async (collaboratorId, body) =>
     HttpRequest.patch(`/collaborator/${collaboratorId}`, body),
+
   deleteCollaborator: async (collaboratorId) =>
     HttpRequest.delete(`/collaborator/${collaboratorId}`),
   createCollaborator: async (data) =>
     HttpRequest.post("/collaborator/create", data),
+  getAllCountries: async () => HttpRequest.get("/countries"),
 };
