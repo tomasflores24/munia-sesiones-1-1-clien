8b8ee8847e9705500022b1376b5a5f35e5d46146
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2023-10-16 15:17:47 -0300

    new changes

---
+++ src/app/materialUITheme.js
@@ -0,0 +1,16 @@
+import { createTheme } from "@mui/material";
+
+export const appTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#ae7a6c'
+        },
+        secondary: {
+            main: '#ebdcc1',
+        },
+        tertiary: {
+            main: '#535353'
+        },
+    }
+})
+
