dacdf6a45e8a9ef1b18eba34c93619aa039c7034
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2023-10-16 17:24:10 -0300

    Loading spinner start/end feature

--- src/app/shared/loadingSpinner/LoadingSpinner.jsx
+++ src/app/shared/loadingSpinner/LoadingSpinner.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import "./LoadingSpinnerStyle.scss";
 
-const LoadingSpinner = () => {
+const LoadingSpinner = ({start, end}) => {
   return (
-    <div className="loading__wrapper">
+    <div className={start ? `loading__wrapper__${start}` : end ? `loading__wrapper__${end}` : "loading__wrapper" }>
       <div className="lds__roller">
         <div></div>
         <div></div>
