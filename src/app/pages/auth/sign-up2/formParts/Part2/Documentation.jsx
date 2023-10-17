44512b0caf03a651c65892b77c424537999e8004
DanielRicra <daniel.int@outlook.com>
2023-10-17 15:51:35 -0500

    Se integró validación con yup

--- src/app/pages/auth/sign-up2/formParts/Part2/Documentation.jsx
+++ src/app/pages/auth/sign-up2/formParts/Part2/Documentation.jsx
@@ -2,9 +2,85 @@ import { useForm } from "react-hook-form";
 import PropTypes from "prop-types";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button } from "@mui/material";
+import InputLabel from "@mui/material/InputLabel";
+import { styled } from "@mui/material/styles";
+
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 import "./DocumentationStyle.scss";
 import { setParts } from "../../../../../redux/slices/registerSlice/registerSlice";
+
+const filesSchema = yup.object({
+  dniDoc: yup
+    .mixed()
+    .test("required", "Debe proporcionar Documento de DNI", (value) => {
+      return value && value.length;
+    })
+    .test("fileSize", "El archivo tiene que ser menos de 2MB", (value) => {
+      return value && value[0] && value[0].size <= 2000000;
+    }),
+  universityDegree: yup
+    .mixed()
+    .test(
+      "required",
+      "Debe proporcionar Documento de Titulación Universitaria",
+      (value) => {
+        return value && value.length;
+      }
+    )
+    .test("fileSize", "El archivo tiene que ser menos de 2MB", (value) => {
+      return value && value[0] && value[0].size <= 2000000;
+    }),
+  masterDegree: yup
+    .mixed()
+    .test("fileSize", "El archivo tiene que ser menos de 2MB", (value) => {
+      return value?.[0] ? value[0].size <= 2000000 : true;
+    }),
+  curriculum: yup
+    .mixed()
+    .test(
+      "required",
+      "Debe proporcionar Documento de Curriculum",
+      (value) => {
+        return value && value.length;
+      }
+    )
+    .test("fileSize", "El archivo tiene que ser menos de 2MB", (value) => {
+      return value && value[0] && value[0].size <= 2000000;
+    }),
+  profesionalCard: yup
+    .mixed()
+    .test("fileSize", "El archivo tiene que ser menos de 2MB", (value) => {
+      return value?.[0] ? value[0].size <= 2000000 : true;
+    }),
+  bankCertification: yup
+    .mixed()
+    .test(
+      "required",
+      "Debe proporcionar Documento de Certificación Bancaria",
+      (value) => {
+        return value && value.length;
+      }
+    )
+    .test("fileSize", "El archivo tiene que ser menos de 2MB", (value) => {
+      return value && value[0] && value[0].size <= 2000000;
+    }),
+});
+
+const VisuallyHiddenInput = styled("input")({
+  clip: "rect(0 0 0 0)",
+  clipPath: "inset(50%)",
+  height: 1,
+  overflow: "hidden",
+  position: "absolute",
+  bottom: 0,
+  left: 0,
+  whiteSpace: "nowrap",
+  width: 1,
+});
 
 const Documentation = ({ setStep }) => {
   const [fileNames, setFileNames] = useState({});
@@ -41,13 +117,15 @@ const Documentation = ({ setStep }) => {
         bankCertification: bankCertification.name,
       }),
     }));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   const {
     register,
     handleSubmit,
-    formState: { isValid },
+    formState: { isValid, errors },
   } = useForm({
+    resolver: yupResolver(filesSchema),
     defaultValues: {
       dniDoc: dniDoc.file ? dniDoc.file : "",
       universityDegree: universityDegree.file ? universityDegree.file : "",
@@ -101,143 +179,155 @@ const Documentation = ({ setStep }) => {
 
   return (
     <section className="documentation__root">
-      <h4>Comencemos con la información básica.</h4>
+      <h4 className="subtitle">Comencemos con la información básica.</h4>
       <form
         className="documentation__form"
         onSubmit={handleSubmit(customHandleSubmit)}
       >
         <div className="wrapper-file_input">
-          <h5>DNI documento</h5>
-          <div className="img__container">
-            <div className="upload">
-              <img src="" alt="" className="profile" />
-              <div className="round">
-                <input
-                  type="file"
-                  accept=".pdf"
-                  className="file-input__input"
-                  {...register("dniDoc", {
-                    onChange: (e) => handleChange(e),
-                  })}
-                />
-              </div>
-              <p className="pic-text">
-                {fileNames.dniDoc ? fileNames.dniDoc : "Subir Archivo"}
-              </p>
-            </div>
+          <InputLabel required>DNI documento</InputLabel>
+          <div className="file__container">
+            <Button
+              component="label"
+              variant="contained"
+              fullWidth
+              startIcon={<CloudUploadIcon className="fileBtn_icon" />}
+            >
+              {fileNames.dniDoc ? fileNames.dniDoc : "Subir Archivo"}
+              <VisuallyHiddenInput
+                type="file"
+                accept=".pdf"
+                {...register("dniDoc", {
+                  onChange: (e) => handleChange(e),
+                })}
+              />
+            </Button>
+            {errors?.dniDoc && (
+              <p className="error_text">{errors?.dniDoc?.message}</p>
+            )}
           </div>
         </div>
         <div className="wrapper-file_input">
-          <h5>Título universitario</h5>
-          <div className="img__container">
-            <div className="upload">
-              <img src="" alt="" className="profile" />
-              <div className="round">
-                <input
-                  type="file"
-                  accept=".pdf"
-                  className="file-input__input"
-                  {...register("universityDegree", {
-                    onChange: (e) => handleChange(e),
-                  })}
-                />
-              </div>
-              <p className="pic-text">
-                {fileNames.universityDegree
-                  ? fileNames.universityDegree
-                  : "Subir Archivo"}
-              </p>
-            </div>
+          <InputLabel required>Título universitario</InputLabel>
+          <div className="file__container">
+            <Button
+              component="label"
+              variant="contained"
+              fullWidth
+              startIcon={<CloudUploadIcon className="fileBtn_icon" />}
+            >
+              {fileNames.universityDegree
+                ? fileNames.universityDegree
+                : "Subir Archivo"}
+              <VisuallyHiddenInput
+                type="file"
+                accept=".pdf"
+                {...register("universityDegree", {
+                  onChange: (e) => handleChange(e),
+                })}
+              />
+            </Button>
+            {errors?.universityDegree && (
+              <p className="error_text">{errors?.universityDegree?.message}</p>
+            )}
           </div>
         </div>
         <div className="wrapper-file_input">
-          <h5>Título de maestria</h5>
-          <div className="img__container">
-            <div className="upload">
-              <img src="" alt="" className="profile" />
-              <div className="round">
-                <input
-                  type="file"
-                  accept=".pdf"
-                  className="file-input__input"
-                  {...register("masterDegree", {
-                    onChange: (e) => handleChange(e),
-                  })}
-                />
-              </div>
-              <p className="pic-text">
-                {fileNames.masterDegree
-                  ? fileNames.masterDegree
-                  : "Subir Archivo"}
-              </p>
-            </div>
+          <InputLabel>Título de maestria</InputLabel>
+          <div className="file__container">
+            <Button
+              fullWidth
+              component="label"
+              variant="contained"
+              startIcon={<CloudUploadIcon className="fileBtn_icon" />}
+            >
+              {fileNames.masterDegree
+                ? fileNames.masterDegree
+                : "Subir Archivo"}
+              <VisuallyHiddenInput
+                type="file"
+                accept=".pdf"
+                {...register("masterDegree", {
+                  onChange: (e) => handleChange(e),
+                })}
+              />
+            </Button>
+            {errors?.masterDegree && (
+              <p className="error_text">{errors?.masterDegree?.message}</p>
+            )}
           </div>
         </div>
         <div className="wrapper-file_input">
-          <h5>Currículum</h5>
-          <div className="img__container">
-            <div className="upload">
-              <img src="" alt="" className="profile" />
-              <div className="round">
-                <input
-                  type="file"
-                  accept=".pdf"
-                  className="file-input__input"
-                  {...register("curriculum", {
-                    onChange: (e) => handleChange(e),
-                  })}
-                />
-              </div>
-              <p className="pic-text">
-                {fileNames.curriculum ? fileNames.curriculum : "Subir Archivo"}
-              </p>
-            </div>
+          <InputLabel required>Currículum</InputLabel>
+          <div className="file__container">
+            <Button
+              component="label"
+              variant="contained"
+              fullWidth
+              startIcon={<CloudUploadIcon className="fileBtn_icon" />}
+            >
+              {fileNames.curriculum ? fileNames.curriculum : "Subir Archivo"}
+              <VisuallyHiddenInput
+                type="file"
+                accept=".pdf"
+                {...register("curriculum", {
+                  onChange: (e) => handleChange(e),
+                })}
+              />
+            </Button>
+            {errors?.curriculum && (
+              <p className="error_text">{errors?.curriculum?.message}</p>
+            )}
           </div>
         </div>
         <div className="wrapper-file_input">
-          <h5>Carta profesional</h5>
-          <div className="img__container">
-            <div className="upload">
-              <img src="" alt="" className="profile" />
-              <div className="round">
-                <input
-                  type="file"
-                  accept=".pdf"
-                  className="file-input__input"
-                  {...register("profesionalCard", {
-                    onChange: (e) => handleChange(e),
-                  })}
-                />
-              </div>
-              <p className="pic-text">
-                {fileNames.profesionalCard
-                  ? fileNames.profesionalCard
-                  : "Subir Archivo"}
-              </p>
-            </div>
+          <InputLabel>Carta profesional</InputLabel>
+          <div className="file__container">
+            <Button
+              component="label"
+              variant="contained"
+              fullWidth
+              startIcon={<CloudUploadIcon className="fileBtn_icon" />}
+            >
+              {fileNames.profesionalCard
+                ? fileNames.profesionalCard
+                : "Subir Archivo"}
+              <VisuallyHiddenInput
+                type="file"
+                accept=".pdf"
+                {...register("profesionalCard", {
+                  onChange: (e) => handleChange(e),
+                })}
+              />
+            </Button>
+            {errors?.profesionalCard && (
+              <p className="error_text">{errors?.profesionalCard?.message}</p>
+            )}
           </div>
         </div>
         <div className="wrapper-file_input">
-          <h5>Certificación bancaria</h5>
-          <div className="img__container">
-            <div className="upload">
-              <img src="" alt="" className="profile" />
-              <div className="round">
-                <input
-                  type="file"
-                  accept=".pdf"
-                  className="file-input__input"
-                  {...register("bankCertification", {
-                    onChange: (e) => handleChange(e),
-                  })}
-                />
-              </div>
-              <p className="pic-text">
-                {fileNames.bankCertification
-                  ? fileNames.bankCertification
-                  : "Subir Archivo"}
-              </p>
-            </div>
+          <InputLabel required>Certificación bancaria</InputLabel>
+          <div className="file__container">
+            <Button
+              component="label"
+              variant="contained"
+              fullWidth
+              startIcon={<CloudUploadIcon className="fileBtn_icon" />}
+            >
+              {fileNames.bankCertification
+                ? fileNames.bankCertification
+                : "Subir Archivo"}
+              <VisuallyHiddenInput
+                type="file"
+                accept=".pdf"
+                {...register("bankCertification", {
+                  onChange: (e) => handleChange(e),
+                })}
+              />
+            </Button>
+            {errors?.bankCertification && (
+              <p className="error_text">{errors?.bankCertification?.message}</p>
+            )}
           </div>
         </div>
       </form>
