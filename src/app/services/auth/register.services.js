6790f8a32ad9835cf31a40468ba03e452f4d0164
DanielRicra <daniel.int@outlook.com>
2023-10-17 10:58:03 -0500

    Se añadio sendFiles y modifico sendFile
    Ahora utlizan patchCustomHeaders

--- src/app/services/auth/register.services.js
+++ src/app/services/auth/register.services.js
@@ -2,11 +2,22 @@ import { HttpRequest } from "../HttpRequest";
 
 export const RegisterServices = {
   signUp: async (data) => HttpRequest.post("/auth/sign-up", data),
-  sendFile: async ({ userId, data }) => {
+  sendFile: async ({ userId, data, accessToken }) => {
     const formData = new FormData();
     formData.append("files", data.file[0]);
-    HttpRequest.postCustomHeaders(`/auth/upload-pic/${userId}`, formData, {
+    HttpRequest.patchCustomHeaders(`/auth/upload-pic/${userId}`, formData, {
       "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${accessToken}`
+    });
+  },
+  sendFiles: async ({ userId, data, accessToken }) => {
+    const formData = new FormData();
+    data.forEach((item) => {
+      formData.append("files", item.file, item.desc)
+    });
+    HttpRequest.patchCustomHeaders(`/auth/upload-files/${userId}`, formData, {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${accessToken}`
     });
   },
 };
