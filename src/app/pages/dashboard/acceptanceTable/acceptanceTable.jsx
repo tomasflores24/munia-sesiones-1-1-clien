19e7ea82071ef6a6e8c0aab7948180ff719586fe
Marcos Damián Ezequiel Medina <marquitosmedina1@hotmail.com>
2023-10-17 12:23:25 -0300

    archivos de AcceptanceTable creados

---
+++ src/app/pages/dashboard/acceptanceTable/acceptanceTable.jsx
@@ -0,0 +1,80 @@
+import "./acceptanceTable.scss"
+import { useState } from "react";
+import {
+  useDeleteProvider,
+  useGetProviders,
+} from "../../../hooks/provider/useProviders";
+import LoadingSpinner from "../../../shared/loadingSpinner/LoadingSpinner";
+import TableShared from "../../../shared/table/tableShared";
+import "./ProviderStyle.scss";
+import BanUserModal from "../../../components/BanUserModal/BanUserModal";
+import { Alert } from "@mui/material";
+
+const AcceptanceTable = () => {
+    const { isLoading, isSuccess, data } = useGetProviders();
+    const [showModal, setShowModal] = useState(false);
+    const [providerId, setProviderId] = useState(null);
+
+    const { mutateAsync, isLoading: isLoadingDelete } = useDeleteProvider();
+
+    const onSearch = (event) => {
+        event.preventDefault();
+        const searchData = new FormData(event.target);
+        console.log(searchData.get("search"));
+    };
+
+    const openModal = (userId) => {
+        setProviderId(userId);
+        setShowModal(true);
+    };
+
+    const onDelete = async (message) => {
+        // TODO: hacer algo con el mensage(descripcion de la baja del usuario)
+        console.log({ message });
+        await mutateAsync(providerId);
+        setShowModal(false);
+    };
+
+    return (
+        <div className="providers__root">
+            <header className="providers__header">
+                <h1 className="title">Profesionales</h1>
+                <form className="providers__search_bar" onSubmit={onSearch}>
+                    <input
+                        type="text"
+                        name="search"
+                        placeholder="Buscar a un profesional"
+                        className="search__input"
+                    />
+                    <button type="submit" className="search__button">
+                        Buscar
+                    </button>
+                </form>
+            </header>
+            {isLoading ? (
+                <LoadingSpinner />
+            ) : !isLoading && isSuccess ? (
+                <div className="providers_table">
+                    <TableShared
+                        data={data.data || []}
+                        currentPage="Providers"
+                        headers={appointmentHeaders}
+                        openModal={openModal}
+                    />
+                    {data?.data?.length === 0 && <Alert variant="filled" color="secondary" severity="info">Todavía no hay clientes, crea uno primero</Alert>}
+                </div>
+            ) : <Alert severity="error">No se pudieron cargar los clientes</Alert>
+            }
+            {showModal && (
+                <BanUserModal
+                    handleModal={() => setShowModal(false)}
+                    onDelete={onDelete}
+                    isLoading={isLoadingDelete}
+                    title="Dar de baja proveedor"
+                />
+            )}
+        </div>
+    )
+}
+
+export default AcceptanceTable;
