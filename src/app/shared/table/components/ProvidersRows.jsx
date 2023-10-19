6bb0dace9565787a487b06347d1713d57efb24ea
DanielRicra <daniel.int@outlook.com>
2023-10-19 09:22:38 -0500

    Tabla providers servicios listos

--- src/app/shared/table/components/ProvidersRows.jsx
+++ src/app/shared/table/components/ProvidersRows.jsx
@@ -1,7 +1,7 @@
+import { Chip } from "@mui/material";
 import PropTypes from "prop-types";
 
 const ProvidersRows = ({ data, openModal }) => {
-
   return (
     <>
       <div className="shared_table-body">
@@ -10,10 +10,26 @@ const ProvidersRows = ({ data, openModal }) => {
             <div className="data-row-tag">
               <div className="imageName__wrapper">
                 <img src={provider.user.profilePic} alt="picture" />
-                <p>{provider.user.name} {provider.last_name}</p>
+                <p>
+                  {provider.user.name} {provider.last_name}
+                </p>
               </div>
             </div>
-            <div className="data-row-tag">Servicios</div>
+            <div className="data-row-tag">
+              {provider.provider_assign_service.length === 0 ? (
+                <p>Sin asignar</p>
+              ) : (
+                provider.provider_assign_service.map((service, i) => (
+                  <div key={service.service.name + i} className="service-type-wrapper">
+                    <Chip
+                      label={service.service.name}
+                      size="small"
+                      className="service-type"
+                    />
+                  </div>
+                ))
+              )}
+            </div>
             <div className="data-row-tag">Sesiones agregadas</div>
             <div className="data-row-tag">Calificaciones</div>
             <div className="data-row-tag">{provider.user.email}</div>
